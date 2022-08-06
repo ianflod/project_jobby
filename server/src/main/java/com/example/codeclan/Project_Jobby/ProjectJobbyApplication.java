ddce44a9700f8a516543f4f711710db1e22b5f4e
ianflod <102290897+ianflod@users.noreply.github.com>
2022-08-06 10:15:02 +0100

    First Commit. Set up Client & Server

---
+++ server/src/main/java/com/example/codeclan/Project_Jobby/ProjectJobbyApplication.java
@@ -0,0 +1,13 @@
+package com.example.codeclan.Project_Jobby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectJobbyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectJobbyApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/example/codeclan/Project_Jobby/ProjectJobbyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codeclan.Project_Jobby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectJobbyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
