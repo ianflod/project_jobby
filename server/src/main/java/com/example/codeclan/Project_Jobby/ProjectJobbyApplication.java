a3774e2667fe72c9016a7e059a37a4af18004b21
JoshMcc1 <101259531+JoshMcc1@users.noreply.github.com>
2022-08-08 10:49:15 +0100

    updating the put mapping in controllers

--- server/src/main/java/com/example/codeclan/Project_Jobby/ProjectJobbyApplication.java
+++ server/src/main/java/com/example/codeclan/Project_Jobby/ProjectJobbyApplication.java
@@ -3,11 +3,19 @@ package com.example.codeclan.Project_Jobby;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Date;
+
 @SpringBootApplication
 public class ProjectJobbyApplication {
 
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectJobbyApplication.class, args);
+
+
 	}
 
 }
+
+
