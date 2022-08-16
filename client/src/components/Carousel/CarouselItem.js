f7ceb5cfb50a243436d495ea71bfefa4c626b4fc
CleoBrown <gillmoynagh@hotmail.co.uk>
2022-08-16 15:02:35 +0100

    css update

--- client/src/components/Carousel/CarouselItem.js
+++ client/src/components/Carousel/CarouselItem.js
@@ -1,23 +1,27 @@
 import React from "react";
 import CarouselList from "./CarouselList";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 
-const CarouselItem = ({featuredJob}) => {
+const CarouselItem = ({ featuredJob }) => {
 
     const url = "/" + featuredJob._id;
 
     // console.log(appliedForJob)
 
-    if (!featuredJob){
+    if (!featuredJob) {
         return "Loading..."
-      }
+    }
 
     return (
         <>
-            <p>{featuredJob.jobTitle}</p>
-            <p>{featuredJob.employerName}</p>
-            <Link to = {url}><button>More details</button></Link>
+            <div className="carousel-text">
+                <h4>
+                    <a href={url}>{featuredJob.jobTitle}</a>
+                </h4>
+                <p>{featuredJob.employerName}</p>
+                {/* <Link to={url}><button>More details</button></Link> */}
+            </div>
         </>
     )
 
